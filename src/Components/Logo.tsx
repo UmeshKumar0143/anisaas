3f36fd8130cb5767b027aa707fd9eaa3d639d321
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-10-07 23:22:18 +0530

    made animation for logo mobile

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -7,6 +7,7 @@ import EchoImg from '../assets/images/echo.png'
 import PulseImg from '../assets/images/pulse.png'
 import QuantumImg from '../assets/images/quantum.png'
 import Image from 'next/image'
+import {motion} from 'framer-motion'
 
 const ImgArr = [
     {src: AcmeImg , alt : "AcmeImg"},
@@ -26,11 +27,22 @@ export default function Logo(){
         </div>
         <div className='flex justify-center'>
         <div className="overflow-hidden flex  relative mt-9 before:content-[''] after:content-[''] before:absolute before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] before:top-0 before:w-20 before:h-full after:absolute after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] after:top-0 after:right-0 after:w-5 after:h-full ">
-        <div className='flex gap-16 '>
+        <motion.div 
+        transition={{
+            duration: 10,
+            repeat: Infinity,
+            ease : 'linear'
+        }}
+        initial = {{translateX: 0}}
+        animate = {{translateX: "-50%"}}
+        className='flex gap-16 pr-16 flex-none  '>
             {ImgArr.map(item=>
                 <Image src = {item.src} className='h-8 flex-none w-auto' alt={item.alt}></Image>
             )}
-        </div>
+            {ImgArr.map(item=>
+                <Image src = {item.src} className='h-8 flex-none w-auto' alt={item.alt}></Image>
+            )}
+        </motion.div>
             </div>
             </div>
         </div>
