aa29d3f720a5c2d74ea0eb43fdec3624a91d71af
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-10-08 23:03:15 +0530

    Completed the products animation

--- src/Components/Products.tsx
+++ src/Components/Products.tsx
@@ -1,7 +1,16 @@
+"use client"
 import Image from "next/image"
 import ProductImg from '../assets/images/app-screen.png'
-
+import {motion, useScroll, useTransform} from 'framer-motion'
+import { useRef } from "react"
 export default function Product(){
+    const prdctImg = useRef<HTMLDivElement>(null)
+    const {scrollYProgress} = useScroll({
+        target: prdctImg,
+        offset:['start end' ,'end end']
+    })
+    const rotateX = useTransform(scrollYProgress,[0,1],[15,0]);
+    const opacity = useTransform(scrollYProgress,[0,1],[0.5,1]);
     return <div className=" bg-[linear-gradient(to_bottom,#000,#5D2CA8)] py-[72px]">
         <div className="container  ">
             <div>
@@ -9,9 +18,16 @@ export default function Product(){
                 <div className="max-w-xl mx-auto">
                 <p className="text-center text-xl  text-white/70 mt-5">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes, one task at a time.</p>
                 </div>
-                <div className="flex justify-center items-center">
+                <motion.div
+                ref = {prdctImg}
+                style={{
+                    opacity: opacity,
+                    rotateX: rotateX,
+                    transformPerspective: "800px"
+                }}
+                 className="flex justify-center items-center">
                 <Image src={ProductImg} alt= "Product" className="mt-14" />
-                </div>
+                </motion.div>
             </div>
         </div>
     </div>
